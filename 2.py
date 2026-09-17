0d5102ef4a1cd7409d72043dd4f4e4c6092b3946
BhavyaBhat2005 <bhavyabhat.work@gmail.com>
2026-09-17 14:11:33 +0530

    python functions for greet, sum and py

---
+++ 2.py
@@ -0,0 +1,6 @@
+def greet():
+
+ print("Welcome")
+ print("FY MCA")
+
+greet()
---
+++ 3.py
@@ -0,0 +1,6 @@
+def add (a,b): #parameters of function
+    print("Sum", a+b) 
+    print(a)
+    print(b)
+
+add(5,10)
